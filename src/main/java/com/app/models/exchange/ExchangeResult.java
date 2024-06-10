9bffdcf1cd82df38b1a554bba359a4c45886c201
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-06-10 12:56:59 +0400

    refactor: Removed unused imports

--- src/main/java/com/app/models/exchange/ExchangeResult.java
+++ src/main/java/com/app/models/exchange/ExchangeResult.java
@@ -1,7 +1,5 @@
 package com.app.models.exchange;
 
-import com.app.dto.response.CurrencyDtoResponse;
-import com.app.entity.CurrencyEntity;
 import com.app.entity.ExchangeRateEntity;
 
 import java.math.BigDecimal;
